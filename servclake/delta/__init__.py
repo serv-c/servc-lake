06e2a7ccca49cbe18f38d44dd2fc1e478c786852
Yusuf Ali <ali@yusuf.email>
2024-01-22 01:09:16 +0000

    feat: initial cut for delta

---
+++ servclake/delta/__init__.py
@@ -0,0 +1,84 @@
+import os
+import shutil
+from typing import Any, List, NotRequired, TypedDict, Union
+
+from deltalake import DeltaTable, Schema, write_deltalake
+from servc.com.service import ComponentType, ServiceComponent
+
+
+class Table(TypedDict):
+    name: str
+    schema: Schema
+    partition: Union[List[str], str]
+    mode: NotRequired[str]
+    description: NotRequired[str]
+    custom_metadata: NotRequired[dict]
+
+
+class Delta(ServiceComponent):
+    _type: ComponentType = ComponentType.DATABASE
+
+    _dataPath: str = None
+
+    _tables: List[Table] = []
+
+    _deltaTables: List[DeltaTable] = []
+
+    def __init__(self, datapath: str, tables: List[Table] = []):
+        super().__init__()
+        self._dataPath = datapath
+        self._tables = tables
+
+    def load_tables(self):
+        for index, table in enumerate(self._tables):
+            if len(self._deltaTables) > index:
+                continue
+
+            table_uri = os.path.join(self._dataPath, table["name"])
+            if not os.path.exists(table_uri):
+                os.makedirs(table_uri, exist_ok=True)
+
+            delta_table = DeltaTable.create(
+                table_uri=table_uri,
+                name=table["name"],
+                schema=table["schema"],
+                partition_by=table["partition"],
+                mode=table.get("mode", "ignore"),
+                description=table.get("description", None),
+                custom_metadata=table.get("custom_metadata", None),
+            )
+            self._deltaTables.append(delta_table)
+
+    def add_table(self, table: Table) -> bool:
+        if table["name"] not in [t["name"] for t in self._tables]:
+            self._tables.append(table)
+            self.load_tables()
+            return True
+        return False
+
+    def get_table(self, tableName: str) -> None | DeltaTable:
+        for table in self._deltaTables:
+            if table.metadata().name == tableName:
+                return table
+        return None
+
+    def delete_table(self, tableName: str) -> bool:
+        for index, table in enumerate(self._tables):
+            if table["name"] == tableName:
+                # remove information off lake
+                table_uri = os.path.join(self._dataPath, table["name"])
+                shutil.rmtree(table_uri)
+
+                del self._tables[index]
+                del self._deltaTables[index]
+                return True
+        return False
+
+    def write(
+        self, tableName: str, data: Any, mode: str = "append", **other
+    ) -> DeltaTable | None:
+        table = self.get_table(tableName)
+        if table:
+            write_deltalake(table, data, mode=mode, **other)
+            return table
+        return None
---
+++ tests/test_server.py
@@ -0,0 +1,66 @@
+import os
+import shutil
+import unittest
+
+import pyarrow as pa
+from deltalake import write_deltalake
+
+from servclake.delta import Delta
+
+df = pa.Table.from_pylist(
+    [
+        {"col": "df1", "value": 1},
+        {"col": "asd2", "value": 2},
+        {"col": "asd3", "value": 3},
+        {"col": "asd4", "value": 4},
+        {"col": "asd5", "value": 5},
+    ],
+    schema=pa.schema([pa.field("col", pa.string()), pa.field("value", pa.int32())]),
+)
+
+table = {
+    "name": "mytesttable",
+    "schema": pa.schema([pa.field("col", pa.string()), pa.field("value", pa.int32())]),
+    "partition": "col",
+}
+
+path = "/tmp/iceberg"
+
+
+class TestServer(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.lake = Delta(path)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.lake.delete_table(table["name"])
+        shutil.rmtree(path)
+
+    def test_create_table(self):
+        self.lake.add_table(table)
+
+        metadata = os.listdir(os.path.join(path, table["name"], "_delta_log"))
+        self.assertGreater(len(metadata), 0)
+
+        result = self.lake.add_table(table)
+        self.assertEqual(result, False)
+
+    def test_write_table(self):
+        tbl = self.lake.write(table["name"], df, mode="append")
+        self.assertEqual(len(tbl.to_pandas()), 5)
+
+    def test_get_table(self):
+        tbl = self.lake.get_table("asdasd")
+        self.assertEqual(tbl, None)
+
+        tbl = self.lake.write("asdasd", df)
+        self.assertEqual(tbl, None)
+
+    def test_fake_delete(self):
+        tbl = self.lake.delete_table("asdasd")
+        self.assertEqual(tbl, False)
+
+
+if __name__ == "__main__":
+    unittest.main()
